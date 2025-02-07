e05d065367caa51658b1a53330fbfb36cee50290
chiaki2025 <tinvu13092003@gmail.com>
2025-02-07 15:22:45 +0700

    Update initializingArray.java

--- array/src/initializingArray.java
+++ array/src/initializingArray.java
@@ -3,12 +3,16 @@ public class initializingArray {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int[] array = new int[5];
+        int[] array2 = {2, 3, 4, 5, 10};
         for(int i = 0; i < 5; i++){
             array[i] = scanner.nextInt();
         }
         scanner.close();
         for(int i = 0; i < 5; i++){
-            System.out.println(array[i]);
+            System.out.print(array[i]);
+        }
+        for(int i = 0; i < 5; i++){
+            System.out.println(array2[i]);
         }
     }
 }
